6465bc6152148e2f65287d61064fc9fc63c22bfb
Thiago Ribeiro <trcosta97@hotmail.com>
2023-11-13 09:59:46 -0300

    projeto

---
+++ src/main/java/com/checkpoint/cp3/Cp3Application.java
@@ -0,0 +1,13 @@
+package com.checkpoint.cp3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cp3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/checkpoint/cp3/Cp3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.checkpoint.cp3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cp3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
